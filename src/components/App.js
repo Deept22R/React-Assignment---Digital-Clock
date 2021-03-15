17a397c4bc954fa6d1f062e42ed8d1004d328ade
Deepti <deepti220398@gmail.com>
2021-03-16 00:47:58 +0530

    commit again

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,80 @@
-import React, {Component, useState} from "react";
+// import React, {Component, useState} from "react";
 import "../styles/App.css";
 
-class App extends Component {
-    constructor() {
-      super();
-      this.state = {
-        time: new Date().toLocaleTimeString(),
-      }
-      this.dateFunction = this.dateFunction.bind(this);
+import React, {useState,useEffect} from 'react';
+
+const App = () => {
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
+    const updateTime = () =>{
+        let date = new Date();
+        let updateHour = date.getHours();
+        let updateMinute = date.getMinutes();
+        let updateSecond = date.getSeconds();
+        let updateAMPM = updateHour < 12 ? 'AM' : 'PM';
+        if(updateMinute < 10) updateMinute = `0${updateMinute}`;
+        if(updateSecond < 10) updateSecond = `0${updateSecond}`;
+        let updateTime = `${updateHour}:${updateMinute}:${updateSecond} ${updateAMPM}`;
+        setTime(updateTime);
     }
+    useEffect(()=>{
+                let clearSetInterval = setInterval(updateTime,1000);
+                return () => clearInterval(clearSetInterval);
+            });
+    return (
+        <div className="Clock">
+            <h3 id="time">
+                {time}
+            </h3>
+        </div>
+    );
+};
+
+export default App;
+
+// class App extends Component {
+//     constructor() {
+//       super();
+//       this.state = {
+//         time: new Date().toLocaleTimeString(),
+//       }
+//       this.dateFunction = this.dateFunction.bind(this);
+//     }
   
-    dateFunction() {
-      let date = new Date();
-      let updateHour = date.getHours();
-      let updateMinute = date.getMinutes();
-      let updateSecond = date.getSeconds();
-      let updateAMPM = updateHour < 12 ? 'AM' : 'PM';
-      updateHour=updateHour>12 ? updateHour-12: updateHour;
-      if(updateMinute < 10) updateMinute = `0${updateMinute}`;
-      if(updateSecond < 10) updateSecond = `0${updateSecond}`;
-      let updateTime = `${updateHour}:${updateMinute}:${updateSecond} ${updateAMPM}`;
-      this.setState({time: updateTime});
-    }
+//     dateFunction() {
+    //   let date = new Date();
+    //   let updateHour = date.getHours();
+    //   let updateMinute = date.getMinutes();
+    //   let updateSecond = date.getSeconds();
+//       let updateAMPM = updateHour < 12 ? 'AM' : 'PM';
+//       updateHour=updateHour>12 ? updateHour-12: updateHour;
+//       if(updateMinute < 10) updateMinute = `0${updateMinute}`;
+//       if(updateSecond < 10) updateSecond = `0${updateSecond}`;
+//       let updateTime = `${updateHour}:${updateMinute}:${updateSecond} ${updateAMPM}`;
+//       this.setState({time: updateTime});
+//     }
   
-    componentDidMount() {
-      this.loadInterval = setInterval(this.dateFunction, 1000)
-    }
+//     componentDidMount() {
+//       this.loadInterval = setInterval(this.dateFunction, 1000)
+//     }
   
-    componentWillUnmount() {
-      clearInterval(this.loadInterval);
-    }
+//     componentWillUnmount() {
+//       clearInterval(this.loadInterval);
+//     }
   
-    render() {
-      return(
-        <>
-          <div className="Clock">
-            <h3 id="time">{this.state.time}</h3>
-          </div>
-        </>
-      )
-    }
-  }
+//     render() {
+//       return(
+//         <>
+//           <div className="Clock">
+//             <h3 id="time">{this.state.time}</h3>
+//           </div>
+//         </>
+//       )
+//     }
+//   }
   
    
   
-  export default App;
+//   export default App;
 // let date = new Date();
 // const App = () => {
 //     const [time, setTime] = useState("");
